05d77578ac91949add02c2f977bf40318a353fce
shubham-arora-tricon <shubham.arora@triconinfotech.com>
2019-11-15 15:48:57 +0530

    hsdgfhsgd

---
+++ src/main/java/com/tricon/travisexample/TravisexampleApplication.java
@@ -0,0 +1,13 @@
+package com.tricon.travisexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravisexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravisexampleApplication.class, args);
+	}
+
+}
