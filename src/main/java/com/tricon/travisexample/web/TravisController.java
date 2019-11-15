bb322a3c4749fcd2662be0681d8efee3617d6a0e
shubham-arora-tricon <shubham.arora@triconinfotech.com>
2019-11-15 16:44:00 +0530

    travis updated

---
+++ src/main/java/com/tricon/travisexample/web/TravisController.java
@@ -0,0 +1,15 @@
+package com.tricon.travisexample.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TravisController {
+
+    @GetMapping("/get")
+    @ResponseBody
+    public String getExample(){
+        return "Hello World with Travis";
+    }
+}
